e68d720a3fcbf795bb0cb64bc9ca601b2463ed59
Matthew Wyskiel <developer@mattwyskiel.com>
2023-01-03 10:38:46 -0500

    [infra] Refine assets bucket for different environments

--- infra/stacks/AssetsStack.ts
+++ infra/stacks/AssetsStack.ts
@@ -1,9 +1,18 @@
-import { StackContext, Api, Bucket } from "@serverless-stack/resources";
-import { ExposedAssetBucket } from "./constructs/AssetBucket";
+import { StackContext } from '@serverless-stack/resources';
+import { ExposedAssetBucket } from './constructs/AssetBucket';
 
 export function AssetsStack({ stack }: StackContext) {
-  const bucket = new ExposedAssetBucket(stack, "AssetsBucket", {
-    customDomain: "assets.mattwyskiel.com",
+  const bucket = new ExposedAssetBucket(stack, 'AssetsBucket', {
+    customDomain:
+      stack.stage === 'prod'
+        ? 'assets.mattwyskiel.com'
+        : 'assets-dev.mattwyskiel.com',
+    s3Bucket: {
+      bucketName:
+        stack.stage === 'prod'
+          ? 'com.mattwyskiel.assets'
+          : 'com.mattwyskiel.assets-dev',
+    },
   });
 
   stack.addOutputs({
