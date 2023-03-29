95ce104fd861e1a3e02030f6cc0627a31a44f335
Matthew Wyskiel <developer@mattwyskiel.com>
2023-03-29 16:38:17 -0400

    Add frontend to deployment

--- frontend/stacks/MyStack.ts
+++ frontend/stacks/MyStack.ts
@@ -1,10 +1,32 @@
-import { StackContext, StaticSite } from 'sst/constructs';
+import {
+  StackContext,
+  StaticSite,
+  StaticSiteDomainProps,
+} from 'sst/constructs';
 
 export function Site({ stack }: StackContext) {
   new StaticSite(stack, 'JekyllSite', {
     path: 'web',
     errorPage: '404.html',
     buildOutput: '_site',
-    buildCommand: 'bundle exec jekyll build',
+    buildCommand: 'JEKYLL_ENV=production bundle exec jekyll build',
+    customDomain: domain(stack.stage),
   });
 }
+
+const domain = (stage: string): StaticSiteDomainProps | undefined => {
+  switch (stage) {
+    case 'prod':
+      return {
+        domainName: 'mattwyskiel.com',
+        domainAlias: 'www.mattwyskiel.com',
+      };
+    case 'dev':
+      return {
+        hostedZone: 'mattwyskiel.com',
+        domainName: 'develop.mattwyskiel.com',
+      };
+    default:
+      return undefined;
+  }
+};
