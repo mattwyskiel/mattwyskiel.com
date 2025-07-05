2ac00ec0192c52cfc56e75a532d545e37190f28b
Matthew Wyskiel <matt@mattwyskiel.com>
2025-07-05 15:22:13 -0400

    rename Env variables to reflect SERVER-side nature

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -1,8 +1,8 @@
 import { createClient } from "contentful";
 
 const BASE_URL = "cdn.contentful.com";
-const SPACE_ID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
-const ACCESS_TOKEN = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
+const SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
+const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
 const ENVIRONMENT = "master";
 
 const client = createClient({
